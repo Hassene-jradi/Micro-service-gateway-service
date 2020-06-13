423f3eb25085d51037292743f9b3a53a56328e44
Hassène JRADI <hassene.jradi4@gmail.com>
2020-06-13 17:56:34 +0200

    Initial commit

---
+++ src/main/java/org/sid/GatewayServiceApplication.java
@@ -0,0 +1,44 @@
+package org.sid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayServiceApplication.class, args);
+	}
+
+	@Bean
+	RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r->r
+						.path("/publicCountries/**")
+						.filters(f->f.addRequestHeader("x-rapidapi-host", "restcountries-v1.p.rapidapi.com")
+									.addRequestHeader("x-rapidapi-key", "6531ce0694msh721004932f964a0p1d7efejsn009a36949d46")
+									.rewritePath("/publicCountries/(?<segment>.*)", "/${segment}"))
+						.uri("https://restcountries-v1.p.rapidapi.com")
+						.id("r1"))
+				.route(r->r
+						.path("/exchange/**")
+						.filters(f->f.addRequestHeader("x-rapidapi-host", "currency-exchange.p.rapidapi.com")
+									.addRequestHeader("x-rapidapi-key", "6531ce0694msh721004932f964a0p1d7efejsn009a36949d46")
+									.rewritePath("/exchange/(?<segment>.*)", "/${segment}"))
+						.uri("https://currency-exchange.p.rapidapi.com")
+						.id("r2"))
+				
+				.build();
+	}
+	
+	@Bean
+	DiscoveryClientRouteDefinitionLocator dynamicRoutes(ReactiveDiscoveryClient dc, DiscoveryLocatorProperties dlp) {
+		return new DiscoveryClientRouteDefinitionLocator(dc, dlp);
+	}
+}
